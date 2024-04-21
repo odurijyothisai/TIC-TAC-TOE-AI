a0a8ac074016c43dedd45fba22737a0a60433dba
odurijyothisai <97426724+odurijyothisai@users.noreply.github.com>
2024-04-21 11:39:41 +0530

    Add files via upload

---
+++ Sample2.py
@@ -0,0 +1,44 @@
+| 0 | 1 | 2 |
+| 3 | 4 | 5 |
+| 6 | 7 | 8 |
+
+X's turn. Input move (0-8): 4
+X makes a move to square 4
+|   |   |   |
+|   | X |   |
+|   |   |   |
+
+O makes a move to square 0
+| O |   |   |
+|   | X |   |
+|   |   |   |
+
+X's turn. Input move (0-8): 2
+X makes a move to square 2
+| O |   | X |
+|   | X |   |
+|   |   |   |
+
+O makes a move to square 1
+| O | O | X |
+|   | X |   |
+|   |   |   |
+
+X's turn. Input move (0-8): 3
+X makes a move to square 3
+| O | O | X |
+| X | X |   |
+|   |   |   |
+
+O makes a move to square 5
+| O | O | X |
+| X | X | O |
+|   |   |   |
+
+X's turn. Input move (0-8): 6
+X makes a move to square 6
+| O | O | X |
+| X | X | O |
+| X |   |   |
+
+X wins!
